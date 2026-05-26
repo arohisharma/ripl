3d32498d99733162520b73dccb2393816e2ee9bf
Arohi Sharma <arohisharma@Arohis-MacBook.local>
2026-05-26 16:20:02 +0530

    added setup.py file for deployment purpose

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ripl",
+    version="0.0.1",
+    description="RIPL Frappe App",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
